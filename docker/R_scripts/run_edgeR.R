324e3843c982ae0a19916c6a775db7a560fd67a6
Christian Covington <christian.t.covington@gmail.com>
2019-03-06 15:54:10 -0500

    fix removal of ensembl_id column

--- docker/R_scripts/run_edgeR.R
+++ docker/R_scripts/run_edgeR.R
@@ -271,7 +271,7 @@ main <- function() {
 
     counts = read.csv(counts_file, row.names=1)
     # remove ensembl_id
-    counts <- subset(counts, select = -c('ensembl_id'))
+    counts <- subset(counts, select = -c(ensembl_id))
 
     design = get_simple_design(samples_file)
     rownames(design) <- make.names(str_trim(rownames(design)))
